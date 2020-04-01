aba93cf690f1abdfe18270089a5787b6e2afc21a
Ken Haase <kh@beingmeta.com>
2020-04-01 16:52:36 -0400

    Added smoketest, have scheme target include optscheme which depends on schemetest

---
+++ smoke.scm
@@ -0,0 +1,12 @@
+(applytest 17 + 11 3 3)
+(evaltest 17 ( + 11 3 3))
+(define (p x y) (+ x y 17))
+(applytest 42 p 12 13)
+(evaltest 42 (p 12 13))
+(define twelve 12)
+(applytest 42 p twelve 13)
+(evaltest 42 (p twelve 13))
+(let ((thirteen 13))
+  (applytest 42 p twelve thirteen)
+  (evaltest 42 (p twelve thirteen)))
+
