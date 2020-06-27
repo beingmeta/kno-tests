72a5bee9e7ba486b76b2ef69a0f30c5305a0f7bc
Ken Haase <kh@beingmeta.com>
2020-06-27 15:01:39 -0400

    Updates to testsrv

--- testsrv.scm
+++ testsrv.scm
@@ -1,13 +1,19 @@
-(define testexpr (config 'TESTEXPR '(PLUS 11 99)))
+(define testop '+)
+(define test-args '(11 99))
 
 (define (evaln server n)
-  (let ((s (open-evalserver server)))
-    (dotimes (i n) (neteval s (or testexpr i)))))
+  (let ((s (open-service server)))
+    (dotimes (i n) (apply sevice/call s testop test-args))))
+
+(define (calln np n)
+  (dotimes (i n) (apply np test-args)))
 
 (define (evalmn server m n)
-  (let ((start (elapsed-time))
-	(threads {}))
-    (dotimes (i m) (set+! threads (spawn (evaln server n))))
-    (thread/join threads)
-    (/ (* m n) (- (elapsed-time) start))))
+  (let* ((s (open-service server))
+	 (nplus (netproc s testop)))
+    (let ((start (elapsed-time))
+	  (threads {}))
+      (dotimes (i m) (set+! threads (spawn (evaln server n))))
+      (thread/join threads)
+      (/ (* m n) (- (elapsed-time) start)))))
 
