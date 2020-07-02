838a729e58e82c5ac69646121c079a905ab04f70
Ken Haase <kh@beingmeta.com>
2020-07-02 19:14:51 -0400

    More knosocks tests

--- knosocks.scm
+++ knosocks.scm
@@ -1,10 +1,27 @@
-(use-module 'knosocks)
+(use-module '{knosocks samplefns})
 
 (define (test-with-socket socket)
-  (let* ((server (knosockd/listener "testing" socket))
+  (let* ((server (knosockd/listener socket
+				    [xrefs #(square fib fact)
+				     initclients 3
+				     maxclients 8
+				     nthreads 2
+				     id "testing"]
+				    [square square fib fibi fact facti]))
 	 (thread (thread/call knosockd/run server))
-	 (client (open-service socket)))
-    (list client server thread)))
+	 (client (open-service socket))
+	 (waitcount 200))
+    (applytest 120 service/call client 'fact 5)
+    (applytest 6765 service/call client 'fib 20)
+    (lineout "Done with tests")
+    (knosockd/shutdown! server)
+    (while (and (not (thread/exited? thread)) (>= waitcount 0))
+      (sleep 0.01)
+      (set! waitcount (-1+ waitcount)))
+    (unless (thread/exited? thread)
+      (logwarn |ZombieAlert| 
+	"The thread " thread " didn't exit when its server was shut down"))
+    thread))
 
 ;; (define testop '+)
 ;; (define test-args '(11 99))
