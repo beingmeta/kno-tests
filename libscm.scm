6d0b2bd905a2738ace7b987f5e033f046bd26ce3
Ken Haase <kh@beingmeta.com>
2020-12-22 10:49:56 -0500

    Avoid inconsistent ziptools issue during testing, add some finer grained timing, tstmods targets

--- libscm.scm
+++ libscm.scm
@@ -1,3 +1,5 @@
+(config! 'noziptools #t)
+
 (check-modules '{condense defmacro defstruct dopool engine ezrecords fifo
 		 io/filestream gpath jsonout logctl logger mimetable
 		 mttools packetfns parsetime io/readfile
